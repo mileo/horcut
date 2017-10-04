3e627cad0d7f6e507fbde0f33ee94266a27158d5
Nicolle Cysneiros <nicolle.cysneiros@gmail.com>
2017-10-04 18:19:32 -0300

    Remove static folder config

--- horcut/settings.py
+++ horcut/settings.py
@@ -127,10 +127,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 GRAPHENE = {
